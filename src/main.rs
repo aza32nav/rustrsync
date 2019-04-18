1f8bbf4b1e8264856a3a84da47390209f3eb6f22
aza32nav <ivan32v7@tuta.io>
2019-04-18 14:49:02 -0600

    Adding Color with ansi_term crate

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::io;
 use std::env;
 use std::process::{Command, Stdio};
 
+extern crate ansi_term;
+
+use ansi_term::Colour;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {panic!("Please provide a filename as argument");}
@@ -14,21 +18,31 @@ fn main() {
     let string_content = read_file_to_string(&filename)
         .expect(&msg_err);
 
-    println!("____________________Initiating Process____________________");
+    println!("{}", 
+        Colour::Cyan.bold().paint(
+            "____________________Initiating Process____________________"));
     // Split the file by '\n' and return in a Vec<&str>
     let (origin, destination, folders) = string_to_vector(&string_content);
-    println!("origin: {}",origin);
-    println!("destination: {}", destination);
-    println!("Folders: {:?}", folders);
+    println!("{} {}",
+        Colour::Yellow.paint("origin:"),
+        Colour::Purple.bold().paint(origin));
+    println!("{} {}",
+        Colour::Yellow.paint("destination:"),
+        Colour::Purple.bold().paint(destination));
+    println!("{} {:?}",
+        Colour::Yellow.paint("Folders:"),
+        folders);
 
-    println!("---------------------------------------------------");
+    println!("{}",
+        Colour::Blue.bold().paint("---------------------------------------------------"));
     // Iterate the folders vector to build the commands
     for folder in folders.iter() {
         if folder == &"" {
             continue
         }
         // Show the full command
-        println!("{}", create_command(origin, destination, folder));
+        println!("{}",
+            Colour::Green.bold().paint(create_command(origin, destination, folder)));
         
         // build the full paths for origin and destination
         let originfull = format!("{}{}", origin, folder);
@@ -38,7 +52,9 @@ fn main() {
         cmd_exec(&originfull, &destfull)
     }
 
-    println!("_________________The Process are Finished_________________");
+    println!("{}",
+        Colour::Cyan.bold().paint(
+            "_________________The Process are Finished_________________"));
 }
 
 fn read_file_to_string(filename: &str) -> io::Result<String> {
@@ -75,8 +91,12 @@ fn cmd_exec(originfull: &str, destfull: &str){
             .expect("rsync command failed to start");
 
         let status = cmd.wait();
-        println!("\nProcess complete with status: {:?}", status);
-        println!("---------------------------------------------------");
+        println!("\n{} {:?}",
+            Colour::Blue.bold().paint("Process complete with status:"),
+            status);
+        println!("{}",
+            Colour::Blue.bold().paint(
+                "---------------------------------------------------"));
 }
 
 fn create_command(origin: &str, dest: &str, folder: &str) -> String {
